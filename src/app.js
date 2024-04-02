4342ed68699b096a76ef9e2ff8ffcef2a95b5ca7
Beethove1 <96740916+Beethove1@users.noreply.github.com>
2024-04-02 01:00:26 +0000

    Commit_1

--- src/app.js
+++ src/app.js
@@ -7,5 +7,30 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronouns = ["the", "our", "tiger"];
+  let adjs = ["great", "big", "fast"];
+  let nouns = ["jogger", "racoon", "boots"];
+  let ends = [".com", ".net", ".edu", ".fr", ".ca", ".org"];
+
+  let domainGen = [];
+  for (let pronoun of pronouns) {
+    for (let adj of adjs) {
+      for (let noun of nouns) {
+        for (let end of ends) {
+          domainGen.push(`${pronoun}${adj}${noun}${end}`);
+        }
+      }
+    }
+  }
+
+  //
+
+  let domainGen2 = [];
+  //domainGen2 = domainGen.join("");
+  //document.querySelector(".bodyDom").innerHTML = domainGen2;
+
+  document.querySelector(".bodyDom").innerHTML = `<ol>${domainGen.map(
+    domainGen => `
+<li>${domainGen}</li>`
+  )}</ol>`;
 };
